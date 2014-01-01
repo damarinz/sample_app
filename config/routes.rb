7bbac5abb687e448c022084f5edb39d5a5ba46da
Bungo Tamari <damarinz@gmail.com>
2014-01-01 17:47:44 +0900

    Section 5.5: Finish Layout and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   root to:'static_pages#home'
   match '/contact', to:'static_pages#contact'
   match '/help', to:'static_pages#help'
   match '/about', to:'static_pages#about'
+  match '/signup', to:'users#new'
 end
